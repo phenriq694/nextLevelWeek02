d26db7cd6749d7d16e4a066ea7abbebdc2c912c1
Paulo Henrique <phenriq694@gmail.com>
2020-08-04 21:09:33 -0300

    WEB: Create teacher list layout

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/icons/whatsapp.svg';
+
 import './styles.css';
 
 function TeacherList() {
@@ -25,6 +27,33 @@ function TeacherList() {
           </div>
         </form>  
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/54601930?s=460&u=03030f57c066bf4ae807fa959538af3e83896e3e&v=4" alt="Paulo Henrique"/>
+            <div>
+              <strong>Paulo Henrique</strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora 
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 }
