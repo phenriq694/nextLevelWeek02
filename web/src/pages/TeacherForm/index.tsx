d1f89bd4b9517b14b27642ab9309eea0ccb31597
Paulo Henrique <phenriq694@gmail.com>
2020-08-06 16:18:41 -0300

    WEB: Create TeacherForm Page layout and Styling.

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/icons/warning.svg';
+
+import './styles.css';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      <PageHeader 
+        title="Que incrível que você quer dar aulas." 
+        description="O primeiro passo é preecher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus Dados</legend>
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre as aulas</legend>
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante"/>
+            Importante ! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+      </main>
     </div>
   )
 }
