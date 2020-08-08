473aaafebf450ca7a2b78ca46c0a02d312dbf7bd
Paulo Henrique <phenriq694@gmail.com>
2020-08-08 18:25:06 -0300

    MOBILE: List  favorite teachers on the Favorites page

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput, AppRegistry } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
